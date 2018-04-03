a654ae28f17e89eb64d562bb9be2bb68b58e4a03
Will Dietz <w@wdtz.org>
2018-04-03 11:15:05 -0500

    travis-ci: show contents of /etc, maybe /etc/nix/nix.conf

--- .travis/ci.sh
+++ .travis/ci.sh
@@ -13,5 +13,11 @@ EOF
 # Install updated Nix with memory usage improvements not in official release yet
 nix-env -i /nix/store/4k1h1klvk58mhm6l905hxx9mf85xxmhb-nix-2.0pre6107_3fbaa230
 
+nix run -f channel:nixos-unstable tree -c tree /etc
+
+cat /etc/nix/nix.conf
+exit 1
+
+
 # Build (uses ./default.nix)
 nix-build
