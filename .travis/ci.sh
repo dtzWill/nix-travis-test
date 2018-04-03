891db938941c0c0692eee6054134c7e173630fc1
Will Dietz <w@wdtz.org>
2018-04-02 21:08:39 -0500

    ci.sh: comments, try running build
    We'll see if sandbox works...

--- .travis/ci.sh
+++ .travis/ci.sh
@@ -2,10 +2,13 @@
 
 mkdir -p ~/.config/nix
 
+# Use the ALLVM binary cache in addition to default NixOS cache
+# Also, ensure sandbox support is enabled
 cat > ~/.config/nix/nix.conf <<EOF
 substituters = https://cache.nixos.org https://cache.allvm.org
 trusted-public-keys = gravity.cs.illinois.edu-1:yymmNS/WMf0iTj2NnD0nrVV8cBOXM9ivAkEdO1Lro3U= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
 sandbox = true
 EOF
 
-nix-store -r /nix/store/d2r8lw84vyqy49jnnkj54jxkwpgjpm62-coreutils-8.29-wllvm-allexe
+# Build (uses ./default.nix)
+nix-build
