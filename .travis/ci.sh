721811cb1c24795abce6f4d72a89a3d92a43c45a
Will Dietz <w@wdtz.org>
2018-04-02 21:02:13 -0500

    ci.sh: oops

--- .travis/ci.sh
+++ .travis/ci.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.config/nix
 
-cat ~/.config/nix/nix.conf <<EOF
+cat > ~/.config/nix/nix.conf <<EOF
 binary-caches https://cache.nixos.org https://cache.allvm.org
 trusted-public-keys gravity.cs.illinois.edu-1:yymmNS/WMf0iTj2NnD0nrVV8cBOXM9ivAkEdO1Lro3U= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
 sandbox true
