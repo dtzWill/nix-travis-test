f5f5663eabf60e45f923544fa281c56e74f7da4c
Will Dietz <w@wdtz.org>
2018-04-02 21:03:44 -0500

    config value is substituters

--- .travis/ci.sh
+++ .travis/ci.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/.config/nix
 
 cat > ~/.config/nix/nix.conf <<EOF
-binary-caches https://cache.nixos.org https://cache.allvm.org
+substituters https://cache.nixos.org https://cache.allvm.org
 trusted-public-keys gravity.cs.illinois.edu-1:yymmNS/WMf0iTj2NnD0nrVV8cBOXM9ivAkEdO1Lro3U= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
 sandbox true
 EOF
